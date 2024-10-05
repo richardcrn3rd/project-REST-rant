87a484f578659d8e16d11c36ea312a323522b35b
“richardcrn3rd” <richardcrn3rd@gmail.com>
2024-10-05 18:06:30 -0400

    Corrected Error in Index.js

--- controllers/places.js
+++ controllers/places.js
@@ -3,5 +3,8 @@ const router = require('express').Router()
 router.get('/', (req, res) => {
     res.send ('GET /places')
 })
+router.get('/', (req, res) => {
+    res.send ('GET /places')
+})
 
 module.exports = router
