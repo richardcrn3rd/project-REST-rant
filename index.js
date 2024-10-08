813be163417d46f756915d9e968e210222cbf9a7
“richardcrn3rd” <richardcrn3rd@gmail.com>
2024-10-08 13:45:00 -0400

    Adding default view for layout

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ require ('dotenv').config()
 const express = require('express')
 const app = express()
 
-app.use('/places', require('./controllers/places'));
+app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
     res.send('Hello world!')
 })
 
-// // app.get('*', (req, res) => {
-// //     res.status(404).send('<h1>404 Page</h1>')
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404 Page</h1>')
 
-// })
+})
 app.get('*', function (req, res) {
     res.send(`
         <body style="margin: 0;">
