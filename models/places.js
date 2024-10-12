6a31ca9c65c1ac002bcf32b5d1f2016b213fdb95
“richardcrn3rd” <richardcrn3rd@gmail.com>
2024-10-12 15:59:27 -0400

    Fixed Default import path and broken images

--- models/places.js
+++ models/places.js
@@ -10,7 +10,7 @@
         city: 'Phoenix',
         state: 'AZ',
         cuisines: 'Coffee, Bakery',
-        pic: 'https://www.google.com/imgres?q=Coding%20Cat%20Cafe&imgurl=http%3A%2F%2F67.205.150.83%2Fimages%2F7343-4681-3555.jpg&imgrefurl=https%3A%2F%2Fdropnite.com%2Fmap.php%3Fid%3D33734&docid=jDESR96dpinnwM&tbnid=hohTpUkiN8yqwM&vet=12ahUKEwjf7ZSy4YSJAxWhSP4FHVX9OGwQM3oECBwQAA..i&w=1920&h=1080&hcb=2&ved=2ahUKEwjf7ZSy4YSJAxWhSP4FHVX9OGwQM3oECBwQAA'
+        pic: 'https://i.ytimg.com/vi/ry17X3C2Ams/oar2.jpg'
      }]
       
       
