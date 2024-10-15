e4d2d1881001b648d3a7c76dfe23231e6edbf39c
“richardcrn3rd” <richardcrn3rd@gmail.com>
2024-10-15 16:55:32 -0400

    Adding Bootstrap grid to index page

--- views/error404.jsx
+++ views/error404.jsx
@@ -7,6 +7,10 @@ function error404 () {
           <main>
               <h1>404: PAGE NOT FOUND</h1>
               <p>Oops, sorry, we can't find this page!</p>
+
+              <div>
+                Photo by <img src="/images/luke-miller-LSd5sX9ThiI-unsplash.jpg"/>
+            </div>
           </main>
       </Def>
     )
--- views/home.jsx
+++ views/home.jsx
@@ -5,11 +5,15 @@ function home () {
     return (
       <Def>
           <main>
-              <h1>HOME</h1>
+            <h1>REST-Rant</h1>
+
             <a href="/places">
                 <button className="btn-primary">Places Page</button>
             </a>
 
+            <div>
+                Photo by Brenda Godinez <img src="/images/brenda-godinez-MsTOg6rhRVk-unsplash.jpg" />
+            </div>
           </main>
       </Def>
     )
