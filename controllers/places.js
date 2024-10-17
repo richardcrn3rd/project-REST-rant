1211f2bddc44159d7ea78fbe5a1f1fc384c9e42d
“richardcrn3rd” <richardcrn3rd@gmail.com>
2024-10-17 18:47:46 -0400

    Addeing and rendering a new view

--- controllers/places.js
+++ controllers/places.js
@@ -1,8 +1,14 @@
 const router = require('express').Router()
 const places = require("../models/places")
 
+router.get('/new', (req, res) => {
+    res.render('places/new')
+  })
+
 router.get('/', (req, res) => {
     res.render('places/index', {places})
 })     
-      
+
+
+  
 module.exports = router
