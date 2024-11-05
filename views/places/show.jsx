2c20b2fd377858e7dba7afa668344a0744669591
“richardcrn3rd” <richardcrn3rd@gmail.com>
2024-11-04 21:17:47 -0500

    Show Page_ Edit_Delete Button Fix

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -6,7 +6,7 @@ function show ({place, id}) {
         <Def>
           <main>
             <h1>{place.name}</h1>
-            <a href={`/places/${data.place.id}/edit`} className="btn btn-warning">Edit</a>
+            <a href={`/places/${id}/edit`} className="btn btn-warning">Edit</a>
             <form method="POST" action={`/places/${id}?_method=DELETE`}>
                 <button type="submit" className="btn btn-danger">Delete</button>
             </form>
