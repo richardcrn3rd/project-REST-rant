23289909b4cdf97ea3b0ecfdb6ba8975d99a4340
“richardcrn3rd” <richardcrn3rd@gmail.com>
2025-02-16 17:07:31 -0500

    Installed mongoose dependencies

--- views/default.jsx
+++ views/default.jsx
@@ -5,24 +5,31 @@ function Def (html) {
         <html>
             <head>
                 <title>Title</title>
-                <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css'></link>
-                <link rel='stylesheet' href='/css/style.css'></link>
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" 
+                integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" 
+                crossOrigin="anonymous"
+                />
+                <link rel="stylesheet" href="/css/style.css"/>
             </head>
             <body>
-            <nav>
-                <ul>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/places">PLACES</a></li>
-                    <li><a href="/places/new">Adding Places</a></li>
-                </ul>
-            </nav>
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="/">Home</a>
+                        </li>
+                        <li>
+                            <a href="/places">Places</a>
+                        </li>
+                        <li>
+                            <a href="/places/new">Add Place</a>
+                        </li>
+                    </ul>
+                </nav>
                 {html.children}
             </body>
-            <footer>
-                Copyright 2024 - NC SW DEV
-            </footer>
         </html>
     )
-  }
+}
 
-module.exports = Def;
+
+module.exports = Def
